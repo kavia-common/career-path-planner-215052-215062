39a44ceaffb711401cbab59cfabec9ed9d9fb138
Kavia AI <production@kavia.ai>
2025-11-27 07:53:51 +0000

    Checkpoint 19 - cgf8ab61da

--- career_planner_database/database_backup.sql
+++ career_planner_database/database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict ibg3ajATO8kz3ojsKTmnpeYG8wFJUmqGjSx4PBJKjMgohNIZLvvNG68msMvkaZ2
+\restrict vxeSFdPjWqOGJw1zsmiZgAAQ1fRFzSDpeZkBmUPwfI8RVC4pbSTIIteyXhLA2in
 
 -- Dumped from database version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
 -- Dumped by pg_dump version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
@@ -28,9 +28,9 @@ CREATE DATABASE myapp WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDE
 
 ALTER DATABASE myapp OWNER TO postgres;
 
-\unrestrict ibg3ajATO8kz3ojsKTmnpeYG8wFJUmqGjSx4PBJKjMgohNIZLvvNG68msMvkaZ2
+\unrestrict vxeSFdPjWqOGJw1zsmiZgAAQ1fRFzSDpeZkBmUPwfI8RVC4pbSTIIteyXhLA2in
 \connect myapp
-\restrict ibg3ajATO8kz3ojsKTmnpeYG8wFJUmqGjSx4PBJKjMgohNIZLvvNG68msMvkaZ2
+\restrict vxeSFdPjWqOGJw1zsmiZgAAQ1fRFzSDpeZkBmUPwfI8RVC4pbSTIIteyXhLA2in
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -89,5 +89,5 @@ ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES 
 -- PostgreSQL database dump complete
 --
 
-\unrestrict ibg3ajATO8kz3ojsKTmnpeYG8wFJUmqGjSx4PBJKjMgohNIZLvvNG68msMvkaZ2
+\unrestrict vxeSFdPjWqOGJw1zsmiZgAAQ1fRFzSDpeZkBmUPwfI8RVC4pbSTIIteyXhLA2in
 
